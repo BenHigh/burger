98a766fe4d1b7e41a4e33c755919fb4f54691b79
BenHigh <ben.high@austin.rr.com>
2017-11-14 23:57:29 -0600

    update connection once more

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
 
-INSERT INTO burgers (name) VALUES ('Road Kill');
-INSERT INTO burgers (name) VALUES ('Cheesy Grand-ez');
+INSERT INTO `burgers` (name) VALUES ('Road Kill');
+INSERT INTO `burgers` (name) VALUES ('Cheesy Grand-ez');
